d2d5e9724229236257f6aa5642675e8fb33c658d
wowiwj <35649084@qq.com>
2017-12-10 12:37:35 +0800

    export vender.js and manifest.js in layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,8 @@
     </div>
 
     <!-- Scripts -->
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
     <script src="{{ mix('js/app.js') }}"></script>
     <script>
 
