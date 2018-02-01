aa815782f0e21f74c01bb6644fa5d7a7a21ec0c2
wowiwj <35649084@qq.com>
2018-02-01 20:32:20 +0800

    fixd acticated email description

--- app/Notifications/SendActivatedEmail.php
+++ app/Notifications/SendActivatedEmail.php
@@ -44,12 +44,12 @@ class SendActivatedEmail extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->subject($this->user->name.':您好,欢迎注册SweetCollege,请激活您的邮箱')
+            ->subject($this->user->name.':您好,欢迎注册Note,请激活您的邮箱')
             ->level('success')
             ->greeting('亲爱的 '.$this->user->name)
-            ->line('首先感谢您加入SweetCollege！')
+            ->line('首先感谢您加入Note社区！')
             ->line('请您在发表言论时，遵守当地法律法规。')
-            ->line('糖果校园，发现不同的乐趣。'.url('/'))
+            ->line('Note社区，发现不同的乐趣。'.url('/'))
             ->action('激活您的邮箱', route('email.verify',$this->user->activation_token))
             ->line('如果您之前并没有发起激活请求的话请忽略此邮件');
     }
