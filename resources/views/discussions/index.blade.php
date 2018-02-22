66f3c6c619df3868d709417681605006ecffdcf8
wowiwj <35649084@qq.com>
2018-02-22 16:42:54 +0800

    refactor index page

--- resources/views/discussions/index.blade.php
+++ resources/views/discussions/index.blade.php
@@ -9,24 +9,6 @@
 
         <div class="column is-9">
             <div class="card">
-                <header class="card-header">
-                    <p class="card-header-title">
-                        讨论
-                    </p>
-
-                    <div class="card-header-icon articles-sort">
-                        <a href="/discussions">最新</a>
-                        ·
-                        @auth
-                            <a href="/discussions?by={{ Auth::user()->name }}">我的</a>
-                            ·
-                        @endauth
-
-                        <a href="/discussions?popular=1">热门</a>
-                        ·
-                        <a href="/discussions?uncommented=1">零回复</a>
-                    </div>
-                </header>
                 <div class="card-content nopadding">
                     <div class="content">
                         @forelse($discussions as $discussion)
@@ -46,7 +28,39 @@
 
 
         <div class="column">
-            @include('layouts.partials.category')
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+
+                        <div class="columns is-multiline">
+                            <div class="column is-12 level-item has-text-centered">
+                                <button class="button is-primary" style="width: 100%">提问</button>
+                            </div>
+
+                            <aside class="menu">
+                                <p class="menu-label">
+                                    选择过滤条件
+                                </p>
+                                <ul class="menu-list">
+                                    <li><a href="/discussions">所有问题</a></li>
+                                    <li><a href="/discussions?by={{ Auth::user()->name }}">我的问题</a></li>
+                                    <li><a href="/discussions?popular=1">热门问题</a></li>
+                                    <li><a href="/discussions?uncommented=1">零回复问题</a></li>
+                                </ul>
+                                <p class="menu-label">
+                                    通过分类过滤
+                                </p>
+                                <ul class="menu-list">
+                                    <li><a>Payments</a></li>
+                                    <li><a>Transfers</a></li>
+                                    <li><a>Balance</a></li>
+                                </ul>
+                            </aside>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
 
