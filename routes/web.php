90302ed9278f4b9f401e0b1eae7004f7627ae40b
wowiwj <35649084@qq.com>
2018-02-27 15:03:03 +0800

    refactor route

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,6 @@ Route::get('/', function () {
 });
 
 
-Route::get('/demo', function () {
-
-    return view('demo');
-});
-
-
 Auth::routes();
 
 Route::group(['prefix' => 'admin','namespace' => 'Admin','middleware' => ['auth', 'admin']], function () {
@@ -49,18 +43,21 @@ Route::group(['namespace' => 'Auth'],function (){
 Route::group(['namespace' => 'Web'], function () {
 
     Route::post('login','SessionsController@store')->name('login');
+    Route::post('register','UsersController@store')->name('register');
+    Route::get('notifications','NotificationsController@index')->name('notifications.index');
+    Route::get('email/verify/{token}',['as'=>'email.verify','uses'=>'UsersController@verify']);
 
     Route::get('articles', 'ArticlesController@index');
-
-    Route::get('articles/create', 'ArticlesController@create')->name('discussions.create');
+    Route::get('articles/create', 'ArticlesController@create')->name('articles.create');
     Route::get('articles/{article}/edit', 'ArticlesController@edit')->name('articles.edit');
+    Route::post('articles', 'ArticlesController@store')->name('articles.store');
+    Route::delete('articles/{article}','ArticlesController@destroy')->name('articles.destroy');
 
+    Route::get('discussions','DiscussionsController@index')->name('discussions.index');
+    Route::get('discussions/{discussion}','DiscussionsController@show')->name('discussions.show');
     Route::get('discussions/create', 'DiscussionsController@create')->name('discussions.create');
     Route::get('discussions/{discussion}/edit', 'DiscussionsController@edit')->name('discussions.edit');
 
-    Route::post('articles', 'ArticlesController@store')->name('articles.store');
-    Route::delete('articles/{article}','ArticlesController@destroy')->name('articles.destroy');
-    Route::post('register','UsersController@store')->name('register');
     Route::get('users/{user}','UsersController@show')->name('users.show');
     Route::get('users/{user}/edit','UsersController@edit')->name('users.edit');
     Route::get('users/{user}/editAvatar','UsersController@editAvatar')->name('users.editAvatar');
@@ -68,57 +65,22 @@ Route::group(['namespace' => 'Web'], function () {
 
     Route::get('drafts','DraftsController@index')->name('drafts.index');
     Route::delete('drafts/{draft}','DraftsController@destroy')->name('drafts.destroy');
-
     Route::get('article/draft/{draft}','DraftsController@show')->name('drafts.show');
     Route::get('article/draft/{draft}/edit','DraftsController@edit')->name('drafts.edit');
 
-    Route::get('notifications','NotificationsController@index')->name('notifications.index');
-
-    Route::get('email/verify/{token}',['as'=>'email.verify','uses'=>'UsersController@verify']);
-
-
     Route::get('categories', 'CategoriesController@show');
-
     Route::get('categories/create', 'CategoriesController@create');
     Route::post('categories/store', 'CategoriesController@store')->name('categories.store');
 
-    Route::post('articles/{article}/comments','CommentsController@store')->name('comments.store');
-
-
     Route::get('articles/{category}', 'ArticlesController@index');
     Route::get('articles/{category}/{article}','ArticlesController@show');
-
-    Route::get('discussions','DiscussionsController@index')->name('discussions.index');
-    Route::get('discussions/{discussion}','DiscussionsController@show')->name('discussions.show');
-
 
     Route::get('special_pages/create','SpecialPagesController@create')->name('special_pages.create');
     Route::get('special_pages/{page}/edit','SpecialPagesController@edit')->name('special_pages.edit');
     Route::delete('special_pages/{page}','SpecialPagesController@destroy')->name('special_pages.destroy');
 
-
-
     Route::get('{name}','SpecialPagesController@show')->name('special_pages.show');
-
-
-
-
 
 });
 
 
-
-
-
-/* Dashboard Index */
-
-
-
-
-
-
-//Route::get('/home', 'HomeController@index')->name('home');
-
-//Auth::routes();
-
-//Route::get('/home', 'HomeController@index')->name('home');
