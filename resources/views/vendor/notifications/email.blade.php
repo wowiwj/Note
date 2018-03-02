ad5b1c5bd7d06f04af4143e2a5c09e1409c5e0a1
wowiwj <35649084@qq.com>
2018-03-02 10:43:42 +0800

    update mail tempalte

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -45,14 +45,13 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-Regards,<br>{{ config('app.name') }}
+荣幸之至,<br>{{ config('app.name') }}
 @endif
 
 <!-- Subcopy -->
 @isset($actionText)
 @component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
+如果点击 "{{ $actionText }}" 按钮无效, 请拷贝改链接到您的浏览器进行访问: [{{ $actionUrl }}]({{ $actionUrl }})
 @endcomponent
 @endisset
 @endcomponent
