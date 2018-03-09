ab682515719a7cff4c2043dc1ebff60b89996582
wowiwj <35649084@qq.com>
2018-03-09 15:40:05 +0800

    add course table

---
+++ database/migrations/2018_03_09_152319_create_series_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('skill_id');
+            $table->string('title');
+            $table->string('banner');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+}
---
+++ database/migrations/2018_03_09_152550_create_lessons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('series_id');
+            $table->unsignedInteger('title');
+            $table->string('video_url')->nullable();
+            $table->text('body')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
